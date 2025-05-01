69b3ca853ff0c4e821ac9e5b141e36c283f2089d
1211454975 <1211454975@qq.com>
2025-05-01 21:03:04 +0800

    commit server

---
+++ crowdmarket-server/ruoyi-monitor/src/main/java/com/ruoyi/monitor/utils/job/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.ruoyi.monitor.utils.job;
+
+import org.quartz.JobExecutionContext;
+import com.ruoyi.monitor.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
